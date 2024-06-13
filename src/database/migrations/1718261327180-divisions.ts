5923f26d62ba6bfe0a9ca6ed56502727dfa72495
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2024-06-13 14:01:13 +0700

    feat(migration): create migration divisions table

---
+++ src/database/migrations/1718261327180-divisions.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Divisions1718261327180 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'divisions',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "divisions"`);
+  }
+}
